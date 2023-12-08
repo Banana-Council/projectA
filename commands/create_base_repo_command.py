f82c68a51fa90fe27ffbdb4935a0f16668c67804
Javier de Santiago <jasampa3000@gmail.com>
2023-12-08 17:45:51 +0100

    small changes

--- commands/create_base_repo_command.py
+++ commands/create_base_repo_command.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from result import Result
 import os
 import subprocess
-
+import json
 from github import Github
 from github import Auth
 
@@ -17,10 +17,41 @@ class CreateBaseRepoCommandHandler:
 
     def execute(self, command: CreateBaseRepoCommand) -> Result[None, Exception]:
 
-        client = Github(auth=Auth.Token(command.github_token))
-        _ = client.get_user().create_repo(name=APP_NAME, private=True)
+        base_directory = self.create_base_directory()
 
+        self.create_github_repository(command.github_token)
+
+        self.set_remote_repository(command.github_token, base_directory)
+
+        self.start_tracking(base_directory)
+
+        self.create_config_directory()
+
+        self.write_github_token_in_config_file(command.github_token)
+
+    def create_base_directory(self):
         base_directory = os.path.expanduser(f"~/{APP_NAME}/")
         os.makedirs(base_directory, exist_ok=True)
-        
+        return base_directory
+    
+    def create_github_repository(self, github_token: str):
+        client = Github(auth=Auth.Token(github_token))
+        client.get_user().create_repo(name=APP_NAME, private=True)
+
+    def set_remote_repository(self, github_token: str, base_directory: str):
+        github_client = Github(auth=Auth.Token(github_token))
+        remote_repo_ssh_url = github_client.get_user().get_repo(APP_NAME).ssh_url
+        subprocess.run(['git', 'remote', 'add', 'origin', remote_repo_ssh_url], cwd=base_directory)
+
+    def start_tracking(self, base_directory):
         subprocess.run(['git', 'init'], cwd=base_directory)
+
+    def create_config_directory(self):
+        config_directory = os.path.expanduser(f"~/.{APP_NAME}config/")
+        os.makedirs(config_directory, exist_ok=True)
+
+    def write_github_token_in_config_file(self, github_token: str):
+        config_file_path = os.path.expanduser(f"~/.{APP_NAME}config/config.txt")
+        config_file = open(config_file_path, 'w')
+        config_file.write(json.dumps({"github_token": github_token}))
+        config_file.close()
--- commands/track_new_file_command.py
+++ commands/track_new_file_command.py
@@ -5,9 +5,6 @@ import shutil
 import subprocess
 from datetime import datetime
 
-from github import Github
-from github import Auth
-
 APP_NAME = "projecta"
 
 @dataclass
@@ -22,15 +19,10 @@ class TrackNewFileHandler:
 
         base_directory = self.get_base_directory()
 
-        tracking_directory = self.create_tracking_directory(command.alias)
-
-        tracking_file = self.create_tracking_file(tracking_directory, command.tracked_file_path)
-
-        self.copy_file(tracking_directory, command.tracked_file_path, tracking_file)
+        tracking_file = self.create_tracking_file(command.alias, command.tracked_file_path)
 
         self.commit_updates(base_directory, tracking_file)
 
-        self.set_remote_repository(base_directory, command.alias, command.github_token)
 
         self.push_updates(base_directory)
 
@@ -40,25 +32,21 @@ class TrackNewFileHandler:
     def create_tracking_directory(self, alias: str) -> str:
         return os.path.expanduser(f"~/{APP_NAME}/{alias}/")
     
-    def create_tracking_file(self, tracking_directory: str, tracked_file_path: str) -> str:
-        return os.path.join(tracking_directory, os.path.basename(tracked_file_path))
+    def create_tracking_file(self, alias: str, tracked_file_path: str) -> str:
+        tracking_directory = os.path.expanduser(f"~/{APP_NAME}/{alias}/")
+        tracking_file_path = os.path.join(tracking_directory, os.path.basename(tracked_file_path))
 
-    def copy_file(self, tracking_directory: str, tracked_file_path: str, tracking_file: str) -> str:
         os.makedirs(tracking_directory, exist_ok=True)
-        shutil.copy2(tracked_file_path, tracking_file)
+        shutil.copy2(tracked_file_path, tracking_file_path)
+
+        return tracked_file_path
 
     def commit_updates(self, base_directory: str, tracking_file: str) -> None:
-        # subprocess.run(['git', 'init'], cwd=tracking_directory)
         subprocess.run(['git', 'add', tracking_file], cwd=base_directory)
 
         commit_message = f'update {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
 
         subprocess.run(['git', 'commit', '-m', commit_message], cwd=base_directory)
-
-    def set_remote_repository(self, base_directory: str, alias: str, github_token: str) -> None:
-        github_client = Github(auth=Auth.Token(github_token))
-        remote_repo_ssh_url = github_client.get_user().get_repo(APP_NAME).ssh_url
-        subprocess.run(['git', 'remote', 'add', 'origin', remote_repo_ssh_url], cwd=base_directory)
 
     def push_updates(self, base_directory: str) -> None:
         subprocess.run(['git', 'push', '-u', 'origin', 'master'], cwd=base_directory)
